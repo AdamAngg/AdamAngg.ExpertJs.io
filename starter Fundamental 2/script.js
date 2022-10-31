4c0893aca87a9c0790edf01b79353637a91e1390
Adam Anglart <116639403+AdamAngg@users.noreply.github.com>
2022-10-31 21:15:52 +0100

    Add files via upload

--- starter Fundamental 2/script.js
+++ starter Fundamental 2/script.js
@@ -50,7 +50,7 @@ const calcAge = () => {
     return `${firstname} have ${retAge} more years till retirement`;
 }
 const age = calcAge();
-console.log(age);*/
+console.log(age);*
 
 
 function cutFruitPieces(fruit) {
@@ -67,6 +67,39 @@ function fruitProcessor(apples, oranges) {
 const result = fruitProcessor(2, 3);
 console.log(result);
 
+function yearsUntil(birthYear) {
+    const yearsUntil = 65 - (2022 - birthYear);
+    return yearsUntil;
+}
+
+function yearUntilRetirement(birthYear, firstname) {
+    const retirementYears = yearsUntil(birthYear);
+    return `urodzony w ${birthYear} ${firstname} bedzie musiał czekać jeszcze ${retirementYears}`;
+}
+
+const result = yearUntilRetirement(1998, `Adam`);
+console.log(result);*/
+
+
+const calcAverage = (x, y, z) => {
+
+    return (x + y + z) / 3;
+};
+function checker() {
+    const dolphinResult = calcAverage(85, 54, 41);
+    const koalasResult = calcAverage(23, 34, 27);
+    if (dolphinResult >= koalasResult * 2) {
+        console.log(`Wygrały delfiny bo ${dolphinResult} jest dwa razwy wiekszy niz kolala result ${koalasResult}`);
+    }
+    else if (koalasResult >= dolphinResult * 2) {
+        console.log(`No koala wygrał bo ${koalasResult} jest dwa razy wieksza niz doplfinow ${dolphinResult}`);
+    }
+    else {
+        console.log(`no nikt nie wygrał bo wynik jakis taki nie podwojony`);
+    }
+}
+
+checker();
 
 
 
