d3062d83ca4bff067026c35322dc12710feb6345
Adam Anglart <116639403+AdamAngg@users.noreply.github.com>
2022-11-03 18:13:50 +0100

    Add files via upload

---
+++ Programowanie/starter Fundamental 1/assignments.js
@@ -0,0 +1,26 @@
+const country = "Poland";
+const continent = "europe";
+let population = 39000000;
+const description = 'Portuagal is in Europe, and its 11 milion people speak portuguese';
+console.log(country);
+console.log(continent);
+console.log(population)
+
+const isIsland = false;
+let language = 'Polish';
+
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof language);
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+const avaregePopulation = 33000000;
+console.log(population - avaregePopulation);
+
+const newDecription = `${country} is in ${continent} and its ${population} people speak ${language}`;
+
+console.log(newDecription);
+
---
+++ Programowanie/starter Fundamental 1/script.js
@@ -0,0 +1,157 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+// Variables called conventions 
+
+;*/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof 'javascriptIsFun');
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+
+javascriptIsFun = 'YES';
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; 
+
+const job;
+let year = 2037;
+const ageJonas = year - 1991;
+const ageSarah = year - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+//2**3 do potęgi trzeciej
+const firstName = 'Jonas';
+const lastName = 'Smitch';
+const space = " ";
+console.log(firstName + space + lastName);
+// opertaor 
+let x = 10 + 5;
+x += 10; // x = x + 10; 
+x *= 4;
+x++; // x = x + 1 
+x-- // x = x - 1 
+console.log(x);
+
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+
+let year = 2037;
+const ageJonas = year - 1991;
+const ageSarah = year - 2018;
+console.log(year - 1991 > year - 2018);
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah);
+console.log(averageAge); 
+
+let markWeight = 95;
+let markaHeight = 1.88;
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+const markBmi = markWeight / (markaHeight * markaHeight);
+const johnBmi = johnWeight / (johnHeight * johnHeight);
+
+const markHigherBmi = markBmi > johnBmi;
+console.log(markHigherBmi); 
+
+const firstName = 'Adam';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+const jonas = "Jestem " + firstName + " moją praca jest " + job + "mam " + (year - birthYear) + " lat";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${job}, jest moją praca. Mam ${year - birthYear} lat`;
+console.log(jonasNew);
+
+const age = 15;
+const oldEnough = age >= 18;
+
+if (oldEnough) {
+    console.log(`Sara może zacząć kurs na prawo jazdy 😂`)
+}
+else {
+    console.log(`Sara nie może iść jeszcze na kurs kurde no ✔`)
+}
+
+let century;
+
+const birthYear = 2012;
+
+if (birthYear <= 2000) {
+    century = 20;
+}
+else {
+    century = 21;
+}
+console.log(century);
+
+
+let markWeight = 95;
+let markaHeight = 1.88;
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+const markBmi = markWeight / (markaHeight * markaHeight);
+const johnBmi = johnWeight / (johnHeight * johnHeight);
+
+const markHigherBmi = markBmi > johnBmi;
+console.log(markHigherBmi);
+
+if (markHigherBmi) {
+    console.log(`Mark has higher BMI`)
+}
+else {
+    console.log(`John has higher BMI`)
+}
+let favorite = prompt(`Co tam podaj liczbe`);
+
+
+
+favorite = Number(favorite);
+console.log(favorite);
+
+if (!isNaN(favorite)) console.log(`super`); 
+
+const Dolphins = (96 + 108 + 89) / 3;
+const Koalas = (88 + 91 + 110) / 3;
+console.log(Dolphins, Koalas)
+const score = 100;
+
+if (Dolphins > Koalas && Dolphins >= score) {
+    console.log(`Delfiny lepsze`);
+} else if (Koalas > Dolphins && Koalas >= score) {
+    console.log(`Koala lepsza`)
+}
+else if (Koalas === Dolphins && Koalas >= score) {
+    console.log(`Remis był`);
+}
+else {
+    console.log(`Nikt nie zdobył średniej 100`);
+}
+
+const day = Number(prompt());
+
+const if8 = day >= 18 ? `masz 18lol` : `hej nie masz`;
+console.log(if8);
+
+console.log(`Lubie chlac wode ale mam 18? ${if8}`);
+
+
+const tip = Number(prompt());
+
+const muchTip = tip >= 50 && tip <= 300 ? tip * 0.15 : tip * 0.20;
+console.log(`Zapłaciłem za obiad ${tip} mój napiwek z tego wynosił ${muchTip} razem zapłaciłem ${tip + muchTip}`);*/
